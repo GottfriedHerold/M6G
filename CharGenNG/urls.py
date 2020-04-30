7c30484bb4604f518ec91eb3817559dae11b7792
Gottfried Herold <gottfried.herold@rub.de>
2020-04-30 14:43:56 +0200

    Added DBInterface app.

--- CharGenNG/urls.py
+++ CharGenNG/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cgtest/', include('CGSandbox.urls')),
+    path('db/', include('DBInterface.urls')),
 ]
