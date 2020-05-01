5e41ae7a4a0c77c877cc1dff6ea596e204112b89
Gottfried Herold <gottfried.herold@rub.de>
2020-05-01 04:19:46 +0200

    Created custom user model. Groups don't display in the admin interface the way they should.

--- DBInterface/admin.py
+++ DBInterface/admin.py
@@ -1,3 +1,62 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+#from django.contrib.auth.forms import UserCreationForm
+from .models import CGUser
+
+class CGUserCreationForm(forms.ModelForm):
+    class Meta:
+        model = CGUser
+        fields = ('username', 'email')
+
+    password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Retype password', widget=forms.PasswordInput)
+
+    def clean_password2(self):
+        """
+            validation of input to password2, called from super().clean()
+        """
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1!=password2:
+            raise forms.ValidationError("Passwords do not match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+class CGUserChangeForm(forms.ModelForm):
+    class Meta:
+            model = CGUser
+            fields = ('username', 'email', 'password', 'is_active', 'is_admin', 'groups')
+
+    def clean_password(self):
+        return self.initial['password']
+
+
+class UserAdmin(BaseUserAdmin):
+    #add_form = CGUserCreationForm
+    #form = CGUserChangeForm
+    list_display = ('username', 'email', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_admin','is_active')}),
+        #('Groups', {'fields':('groups',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username'),
+    filter_horizontal = ()
 
 # Register your models here.
+admin.site.register(CGUser, UserAdmin)
--- DBInterface/models.py
+++ DBInterface/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group
 
 # Create your models here.
+
+class CGUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        """
+            Creates a standard (non-admin) user
+        """
+        if not username:
+            raise ValueError('Username is empty')
+        user = self.model(username=username, email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username=username, email=email, password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class CGUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=40, unique=True)
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    objects = CGUserManager()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    email = models.EmailField(verbose_name='email address', max_length=255)
+    groups = models.ManyToManyField(Group)
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_staff(self):
+        """
+            Used by Django's built-in Admin (web-)interface to determine whether the user has access to that interface.
+            Note that is_superuser is used internally by Django to override its default permission system for model/object-
+            specific permissions. These are, in general, different things, hence Django distinguishes staff and admin.
+        """
+
+        return self.is_admin
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
